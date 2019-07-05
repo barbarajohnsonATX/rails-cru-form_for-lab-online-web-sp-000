486553b2e110f0fe49fb67de159635962f8907b1
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-05 01:16:15 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,19 +1,33 @@
 class GenresController < ApplicationController
-  def new
+   def new
+    @genre = Genre.new 
   end
 
   def create
+    @genre = Genre.create(strong_params(:name))
+    redirect_to genre_path(@genre)
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.update(strong_params(:name))
+    redirect_to genre_path(@artist)
   end
 
   def index
+    @genres = Genre.all 
   end
 
   def show
+    @genres = Genre.find(params[:id])
+  end
+  
+  private
+
+  def strong_params(*args)
+      params.require(:genre).permit(*args)
   end
 end
