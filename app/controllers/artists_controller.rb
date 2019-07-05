3d7f49fbba0ded92ba89394f4373c87910ca1ea1
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-05 01:07:09 +0000

    Automatically backed up by Learn

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,34 @@
+class ArtistsController < ApplicationController
+  def new
+    @artist = Artist.new 
+  end
+
+  def create
+    @artist = Artist.create(strong_params(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.update(strong_params(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+
+  def index
+    @artists = Artist.all 
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+  
+    private
+
+    def strong_params(*args)
+        params.require(:artist).permit(*args)
+    end
+    
+end
