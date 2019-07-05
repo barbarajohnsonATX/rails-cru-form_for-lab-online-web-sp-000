f563f2109ad0e37cdee0788daf9273075e4f8f9f
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-05 01:24:00 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,33 @@
 class SongsController < ApplicationController
-   
+  def new
+    @song = Song.new 
+  end
+
+  def create
+    @song = Genre.create(strong_params(:name))
+    redirect_to genre_path(@song)
+  end
+
+  def edit
+    @song = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.update(strong_params(:name))
+    redirect_to genre_path(@genre)
+  end
+
+  def index
+    @genres = Genre.all 
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+  
+  private
+
+  def strong_params(*args)
+      params.require(:genre).permit(*args)
+  end   
 end
