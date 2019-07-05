4f48360087a0d03207f86654de2feb06d87ac082
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-05 01:23:16 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,19 +1,3 @@
 class SongsController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def index
-  end
-
-  def show
-  end
+   
 end
